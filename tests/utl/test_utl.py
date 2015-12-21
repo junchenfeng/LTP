a06c64d87e9033ed9b005da58125c617f2ba951d
WANG Lei <wlbksy@126.com>
2015-12-21 16:34:10 +0800

    fix test

--- tests/utl/test_utl.py
+++ tests/utl/test_utl.py
@@ -21,19 +21,20 @@ class TestAssembly(unittest.TestCase):
         self.assertTrue(abs(l - true_l) < 1e-15)
 
     def test_Z(self):
-        learning_curve_matrix = np.array([[0.25, 0.1], 
+        learning_curve_matrix = np.array([[0.25, 0.1],
                                           [0.50, 0.2],
                                           [0.75, 0.3]]).reshape(3, 2)
         mixture_density = np.array([0.2, 0.8]).reshape(2, 1)
         z = utl.Z_assembly(self.response_list, learning_curve_matrix, mixture_density)
 
         sum_z = 0.2*0.75*0.5*0.25+0.8*0.9*0.8*0.7
-        true_z = np.array([0.2*0.75*0.5*0.25/sum_z, 0.8*0.9*0.8*0.7/sum_z]).reshape(2,1)
+        true_z = np.array([0.2*0.75*0.5*0.25/sum_z, 0.8*0.9*0.8*0.7/sum_z])
+
         self.assertTrue(sum(abs(true_z - z)) < 1e-15)
 
 
     def test_mixture_update(self):
-        learning_curve_matrix = np.array([[0.1, 0.9], 
+        learning_curve_matrix = np.array([[0.1, 0.9],
                                           [0.1, 0.9],
                                           [0.1, 0.9]]).reshape(3, 2)
         mixture_density = np.array([0.5, 0.5]).reshape(2, 1)
@@ -41,7 +42,7 @@ class TestAssembly(unittest.TestCase):
         ps = utl.update_mixture_density([self.response_list, self.response_list],
                 learning_curve_matrix, mixture_density)
         self.assertTrue(ps[0]>0.995)
-        
+
 
 
 
